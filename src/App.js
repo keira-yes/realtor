a1d7c67f3ba5985f02af69b0987e61e49af8b770
Irina Kirillova <keira.kirillova@gmail.com>
2022-12-20 22:03:36 +0100

    add react-router-dom, add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Apartments from "./pages/Apartments";
+import HotOffers from "./pages/HotOffers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
   return (
-    <h1>Realtor</h1>
+    <>
+      <Router>
+        <Routes>
+            <Route path='/' element={<Apartments />} />
+            <Route path='/hot-offers' element={<HotOffers />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/HotOffers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotOffers = () => {
+    return (
+        <div>
+            <h1>Hot Offers</h1>
+        </div>
+    );
+};
+
+export default HotOffers;
