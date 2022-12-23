98cd7e45fca921ee421e255000916ad2c7f66392
Irina Kirillova <keira.kirillova@gmail.com>
2022-12-23 16:09:30 +0100

    add normalize.css, add SignIn and SignUp page elements

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,73 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import logo from "../assets/img/logo.svg";
+
 const SignIn = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const [showPassword, setShowPassword] = useState(false);
+
+    const { email, password } = formData;
+
+    const onChangeInput = e => {
+        setFormData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
     return (
-        <div>
-            <h1>Sign In</h1>
+        <div className="sign-in">
+            <div className="auth-page">
+                <div className="auth-page__container container">
+                    <header className="auth-page__header">
+                        <Link to="/" className="auth-page__logo">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                        <h1 className="auth-page__title">Sign In</h1>
+                    </header>
+                    <div className="auth-page__form">
+                        <form className="form" name="sign-in">
+                            <div className="form__fields">
+                                <div className="form__field">
+                                    <input
+                                        type="email"
+                                        className="form__field-input email-input"
+                                        name="email"
+                                        value={email}
+                                        onChange={onChangeInput}
+                                    />
+                                </div>
+                                <div className="form__field">
+                                    <input
+                                        type={showPassword ? 'text' : 'password'}
+                                        className="form__field-input password-input"
+                                        name="password"
+                                        value={password}
+                                        onChange={onChangeInput}
+                                    />
+                                    <button type="button" className="form__field-button password-button">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <g>
+                                                <path d="M12 7C8.36364 7 5.25818 9.26182 4 12.4545C5.25818 15.6473 8.36364 17.9091 12 17.9091C15.6364 17.9091 18.7418 15.6473 20 12.4545C18.7418 9.26182 15.6364 7 12 7ZM12 16.0909C9.99273 16.0909 8.36364 14.4618 8.36364 12.4545C8.36364 10.4473 9.99273 8.81818 12 8.81818C14.0073 8.81818 15.6364 10.4473 15.6364 12.4545C15.6364 14.4618 14.0073 16.0909 12 16.0909ZM12 10.2727C10.7927 10.2727 9.81818 11.2473 9.81818 12.4545C9.81818 13.6618 10.7927 14.6364 12 14.6364C13.2073 14.6364 14.1818 13.6618 14.1818 12.4545C14.1818 11.2473 13.2073 10.2727 12 10.2727Z" />
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="form__submit">
+                                <button type="submit" className="form__submit-btn">Log in</button>
+                            </div>
+                        </form>
+                        <Link to="/forgot-password" className="auth-page__link">Forgot your password?</Link>
+                    </div>
+                    <footer className="auth-page__footer">
+                        <Link to="/sign-up" className="auth-page__button shadow-btn">Don't have an account yet? Sign up!</Link>
+                    </footer>
+                </div>
+            </div>
         </div>
     );
 };
