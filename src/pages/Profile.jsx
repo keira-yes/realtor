b8090d549fe49b8fb85d108387c14c0d7fdca149
Irina Kirillova <keira.kirillova@gmail.com>
2022-12-25 15:35:11 +0100

    add user info Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+
 const Profile = () => {
+    const [user, setUser] = useState(null);
+
+    const auth = getAuth();
+
+    useEffect(() => {
+        setUser(auth.currentUser);
+    }, []);
+
     return (
         <div>
             <h1>Profile</h1>
+            {user ? user.displayName : "Please, log in..."}
         </div>
     );
 };
