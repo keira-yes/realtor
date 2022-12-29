d950b5228adbd6b9290efb8aeb0a3e8dc8641bf4
Irina Kirillova <keira.kirillova@gmail.com>
2022-12-30 00:52:28 +0100

    add GoogleAuth

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -1,8 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from 'react-toastify';
 
 const GoogleAuth = () => {
-    const onGoogleAuth = () => {
+    const navigate = useNavigate();
 
+    const onGoogleAuth = async () => {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                await setDoc(doc(db, "users", user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp()
+                });
+            }
+
+            navigate("/profile");
+
+        } catch (error) {
+            toast.error("Something went wrong. Please, try again later");
+        }
     }
+
     return (
         <button type="button" className="google-auth" onClick={onGoogleAuth}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
